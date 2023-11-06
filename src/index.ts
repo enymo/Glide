fec9ac9806f26ee4437b34a1e26469e57fe7f4c8
Gideon Ritter <gideon.ritter@enymo.com>
2023-11-06 13:19:10 +0100

    Add type exports.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-export { default as createGlideInput } from "./Components/Input";
-export { default as createGlideButton } from "./Components/Button"; 
-export { default as createGlideChoice } from "./Components/Choice";
+export { default as createGlideButton } from "./Components/Button";
 export { default as createGlideCheckboxList, useCheckboxList } from "./Components/CheckboxList";
+export { default as createGlideChoice } from "./Components/Choice";
+export { default as createGlideInput } from "./Components/Input";
 export { default as createGlideRadioGroup, useRadioGroup } from "./Components/RadioGroup";
+
+export type { GlideButtonConfig, GlideChoiceConfig, GlideChoiceGroupConfig, GlideInputConfig } from "./types";
